9e062bdd72992c5dbd1ba3e171b1fa58b869fb6c
ASLawan <nawalnitsua@gmail.com>
2024-12-30 11:14:40 +0100

    Create user authentication API endpoints

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { User } from "../models/userModel.js";
 
 const router = express.Router();
@@ -38,6 +39,50 @@ router.post("/signup", async (req, res) => {
     res.status(201).send({
       message: "User created successfully",
       success: true,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      message: error.message,
+      success: false,
+    });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!password || !email) {
+    return res.status(400).send({
+      message: "Incomplete credentials!",
+      success: false,
+    });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).send({
+        message: "Invalid user credentials",
+        success: false,
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(password, user.password);
+    if (!isValidPass) {
+      return res.status(400).send({
+        message: "Invalid user credentials",
+        success: false,
+      });
+    }
+
+    const secret = process.env.SECRET || "Sewoyebaa";
+    const token = jwt.sign({ userId: user._id }, secret, { expiresIn: "1d" });
+
+    res.status(200).send({
+      message: "User logged in successfully",
+      success: true,
+      token: token,
     });
   } catch (error) {
     return res.status(500).send({
